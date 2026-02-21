4fe345b3974ac736841fd20720f5ff95bd56c26c
Juraj Kollár <mostypc7@gmail.com>
2026-02-21 20:44:16 +0100

    fix: whoops (#28)
    * forgot to run make clean
    
    * Reboot instead of shutdown
    
    * switched from RB_POWER_OFF to RB_AUTOBOOT (main.c)
    * edited Makefile to not run installed-vm target because of reboot
    * commented out GRUB patching until we fix it
    
    * remove irrelevant comment
    
    * wip(inst): start migrating to squashfs
    
    also improved some files for absolutely no reason i mean maybe boredom idk
    
    * ci(makefile): add squashfs-tools to dependencies in makefile.yml
    
    * wip(inst): rename the functions in main.c and backend.c (squashfs migration)
    
    * removed grub patch
    * renamed dd_drive -> copy_rootfs
    * renamed iso_to_img -> unsquash
    * added blank 'int install_grub(char* drive)'
    * renamed outputs in main.c
    
    * docs(inst): changed ai comment to yes
    
    * wip(inst): update some function names, unsquash
    
    * minor(inst): update common.h
    
    * chore(build): small changes to Makefile
    
    * improved 'clean'
    * added 'dep'
    
    * a ton of changes me is not gonna list that blud
    
    * design(makefile): make the makefile look a bit too good
    
    * things
    
    * h
    
    * improvement: only partition ESP/MBR on needed cases
    
    * IT WORKS IT WORKS IT WORKS
    
    * downloads only when necxcesarry
    
    * command to run copying of system libs
    
    * WORKS! we now only need tzo patch config for other drives and test uefi
    
    * h
    
    * fix
    
    * login oooo
    
    * Update init
    
    * Update linuxImage
    
    * docs: fixed ai comment
    
    * copy_syslibs fixed (hopefully)
    
    * fixfixfix
    
    * boot target?
    
    * use libcp
    
    * whoops
    
    * whoops

--- copy_syslibs.py
+++ copy_syslibs.py
@@ -6,8 +6,10 @@ print(
 
 os.system("sudo true")
 
+os.system("sudo cp /usr/sbin/grub-install rootfs/filesystem/bin/")
 print("Downloading libcp")
 os.system(
     "curl -L -o libcp https://github.com/redroselinux/libcp/releases/download/v0.1/libcp &&chmod +x libcp"
 )
 os.system("sudo ./libcp rootfs/filesystem/bin/grub-install rootfs/filesystem")
+os.system("sudo cp /usr/lib/grub rootfs/filesystem/lib/")
