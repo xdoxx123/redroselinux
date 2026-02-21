34a82d9a5262d3b3679c4a97b7a98f1eeff595ac
Juraj Kollár <mostypc7@gmail.com>
2026-02-21 20:40:14 +0100

    feat: use libcp (#27)
    * forgot to run make clean
    
    * Reboot instead of shutdown
    
    * switched from RB_POWER_OFF to RB_AUTOBOOT (main.c)
    * edited Makefile to not run installed-vm target because of reboot
    * commented out GRUB patching until we fix it
    
    * remove irrelevant comment
    
    * wip(inst): start migrating to squashfs
    
    also improved some files for absolutely no reason i mean maybe boredom idk
    
    * ci(makefile): add squashfs-tools to dependencies in makefile.yml
    
    * wip(inst): rename the functions in main.c and backend.c (squashfs migration)
    
    * removed grub patch
    * renamed dd_drive -> copy_rootfs
    * renamed iso_to_img -> unsquash
    * added blank 'int install_grub(char* drive)'
    * renamed outputs in main.c
    
    * docs(inst): changed ai comment to yes
    
    * wip(inst): update some function names, unsquash
    
    * minor(inst): update common.h
    
    * chore(build): small changes to Makefile
    
    * improved 'clean'
    * added 'dep'
    
    * a ton of changes me is not gonna list that blud
    
    * design(makefile): make the makefile look a bit too good
    
    * things
    
    * h
    
    * improvement: only partition ESP/MBR on needed cases
    
    * IT WORKS IT WORKS IT WORKS
    
    * downloads only when necxcesarry
    
    * command to run copying of system libs
    
    * WORKS! we now only need tzo patch config for other drives and test uefi
    
    * h
    
    * fix
    
    * login oooo
    
    * Update init
    
    * Update linuxImage
    
    * docs: fixed ai comment
    
    * copy_syslibs fixed (hopefully)
    
    * fixfixfix
    
    * boot target?
    
    * use libcp
    
    * whoops

--- copy_syslibs.py
+++ copy_syslibs.py
@@ -1,43 +1,13 @@
 import os
 
 print(
-    "Hello! This script copies system libraries to the source, so you can test the new installer "
-    "before we build the binaries ourselves. This will get deleted after we build the binaries."
-)
-
-print(
     "We need root permissions to run this script. For that reason, we will run 'sudo true' right now and sudo will remember it."
 )
 
 os.system("sudo true")
 
-os.system("mkdir -p rootfs/filesystem/lib")
-os.system("mkdir -p rootfs/filesystem/lib64")
-os.system("mkdir -p rootfs/filesystem/lib/x86_64-linux-gnu")
-os.system("cp /lib/x86_64-linux-gnu/liblzma.so.5 rootfs/filesystem/lib")
+print("Downloading libcp")
 os.system(
-    "cp /lib/x86_64-linux-gnu/libdevmapper.so.1.02.1 rootfs/filesystem/lib/x86_64-linux-gnu/"
+    "curl -L -o libcp https://github.com/redroselinux/libcp/releases/download/v0.1/libcp &&chmod +x libcp"
 )
-os.system(
-    "cp /lib/x86_64-linux-gnu/libefivar.so.1 rootfs/filesystem/lib/x86_64-linux-gnu/"
-)
-os.system(
-    "cp /lib/x86_64-linux-gnu/libefiboot.so.1 rootfs/filesystem/lib/x86_64-linux-gnu/"
-)
-os.system("cp /lib/x86_64-linux-gnu/libc.so.6 rootfs/filesystem/lib/x86_64-linux-gnu/")
-os.system(
-    "cp /lib/x86_64-linux-gnu/libselinux.so.1 rootfs/filesystem/lib/x86_64-linux-gnu/"
-)
-os.system(
-    "cp /lib/x86_64-linux-gnu/libudev.so.1 rootfs/filesystem/lib/x86_64-linux-gnu/"
-)
-os.system("cp /lib/x86_64-linux-gnu/libm.so.6 rootfs/filesystem/lib/x86_64-linux-gnu/")
-os.system("cp /lib64/ld-linux-x86-64.so.2 rootfs/filesystem/lib64/")
-os.system(
-    "cp /lib/x86_64-linux-gnu/libpcre2-8.so.0 rootfs/filesystem/lib/x86_64-linux-gnu/"
-)
-os.system(
-    "cp /lib/x86_64-linux-gnu/libcap.so.2 rootfs/filesystem/lib/x86_64-linux-gnu/"
-)
-os.system("cp /usr/sbin/grub-install rootfs/filesystem/bin/")
-os.system("cp /usr/lib")
+os.system("sudo ./libcp rootfs/filesystem/bin/grub-install rootfs/filesystem")
