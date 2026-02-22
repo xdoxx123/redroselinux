d01ae40fb6820fd4fdc9b2d4cd0c0cb9e8b74c36
Juraj Kollár <mostypc7@gmail.com>
2026-02-22 15:04:43 +0100

    fix: symlinked busybox to login (ref #24) (#32)
    * forgot to run make clean
    
    * Reboot instead of shutdown
    
    * switched from RB_POWER_OFF to RB_AUTOBOOT (main.c)
    * edited Makefile to not run installed-vm target because of reboot
    * commented out GRUB patching until we fix it
    
    * remove irrelevant comment
    
    * wip(inst): start migrating to squashfs
    
    also improved some files for absolutely no reason i mean maybe boredom idk
    
    * ci(makefile): add squashfs-tools to dependencies in makefile.yml
    
    * wip(inst): rename the functions in main.c and backend.c (squashfs migration)
    
    * removed grub patch
    * renamed dd_drive -> copy_rootfs
    * renamed iso_to_img -> unsquash
    * added blank 'int install_grub(char* drive)'
    * renamed outputs in main.c
    
    * docs(inst): changed ai comment to yes
    
    * wip(inst): update some function names, unsquash
    
    * minor(inst): update common.h
    
    * chore(build): small changes to Makefile
    
    * improved 'clean'
    * added 'dep'
    
    * a ton of changes me is not gonna list that blud
    
    * design(makefile): make the makefile look a bit too good
    
    * things
    
    * h
    
    * improvement: only partition ESP/MBR on needed cases
    
    * IT WORKS IT WORKS IT WORKS
    
    * downloads only when necxcesarry
    
    * command to run copying of system libs
    
    * WORKS! we now only need tzo patch config for other drives and test uefi
    
    * h
    
    * fix
    
    * login oooo
    
    * Update init
    
    * Update linuxImage
    
    * docs: fixed ai comment
    
    * copy_syslibs fixed (hopefully)
    
    * fixfixfix
    
    * boot target?
    
    * use libcp
    
    * whoops
    
    * whoops
    
    * minor: remove the warning that no longer applies
    
    * fixed login and fixed copy_syslibs
    
    #24

--- copy_syslibs.py
+++ copy_syslibs.py
@@ -5,11 +5,16 @@ print(
 )
 
 os.system("sudo true")
-
 os.system("sudo cp /usr/sbin/grub-install rootfs/filesystem/bin/")
-print("Downloading libcp")
-os.system(
-    "curl -L -o libcp https://github.com/redroselinux/libcp/releases/download/v0.1/libcp &&chmod +x libcp"
-)
+
+if not os.path.exists("libcp"):
+    print("Downloading libcp...")
+    os.system(
+        "curl -# -L -o libcp https://github.com/redroselinux/libcp/releases/download/v0.1/libcp"
+    )
+    os.system("chmod +x libcp")
+else:
+    print("libcp already exists, skipping download.")
+
 os.system("sudo ./libcp rootfs/filesystem/bin/grub-install rootfs/filesystem")
-os.system("sudo cp /usr/lib/grub rootfs/filesystem/lib/")
+os.system("sudo cp /usr/lib/grub rootfs/filesystem/lib/ -r")
