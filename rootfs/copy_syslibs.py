ab1f572a649c0feeb6df6068dd8473c6cbcc8e70
mostypc123 <mostypc123@redroselinux.org>
2026-06-05 19:48:46 +0200

    finally standard runit structure + script improved

--- rootfs/copy_syslibs.py
+++ rootfs/copy_syslibs.py
@@ -1,19 +1,26 @@
 import os
+import sys
 
 # was ai used in this script? no
 
 os.system("cp /usr/sbin/grub-install rootfs/filesystem/bin/")
 
 if not os.path.exists("libcp"):
-    print("Downloading libcp...")
+    print("=> Downloading libcp...")
     os.system(
-        "curl -# -L -o libcp https://github.com/redroselinux/libcp/releases/download/v0.1/libcp"
+        "curl -# -L -o libcp"
+        "https://github.com/redroselinux/libcp/releases/download/v0.1/libcp"
     )
     os.system("chmod +x libcp")
 else:
-    print("libcp already exists, skipping download.")
+    pass
 
-os.system("./libcp rootfs/filesystem/bin/grub-install rootfs/filesystem")
-os.system("cp /usr/lib/grub rootfs/filesystem/lib/ -r")
-os.system("mkdir -p rootfs/filesystem/usr/lib/")
-os.system("cp /usr/lib/grub rootfs/filesystem/usr/lib/ -r")
+def execc(command):
+    if os.system(command) != 0:
+        print("failed to run command")
+        sys.exit(1)
+
+execc("./libcp rootfs/filesystem/bin/grub-install rootfs/filesystem")
+execc("cp /usr/lib/grub rootfs/filesystem/lib/ -r")
+execc("mkdir -p rootfs/filesystem/usr/lib/")
+execc("cp /usr/lib/grub rootfs/filesystem/usr/lib/ -r")
